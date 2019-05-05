7a9f8df54257572dffda9fab72b7b8b51a7363a6
林泽鑫 <1046110267@qq.com>
2019-05-05 15:51:00 +0800

    typescript wxapp

--- src/pages/main/home/index.ts
+++ src/pages/main/home/index.ts
@@ -1,13 +1,3 @@
-const { ajax } = getApp();
 Page({
 	data: {},
-	async prevLoad() {
-		const res = await ajax.get('/homeData');
-		this.emitData(res.data);
-	},
-	onLoad() {
-		this.recieveData((data) => {
-			console.log(data);
-		});
-	},
 });
